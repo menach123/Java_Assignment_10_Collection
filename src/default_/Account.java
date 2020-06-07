85482fe6c15b58bdd7527c32091a1104485cc76d
menach123 <mathew.menachery@gmail.com>
2020-06-07 15:19:31 -0400

    First commit

---
+++ src/default_/Account.java
@@ -0,0 +1,35 @@
+package default_;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+	
+	private long id;
+	
+	private String accountType = new String();
+	
+	public Account (long id, String accountType) {
+		this.id = id;
+		this.accountType = accountType;
+	}
+	
+	public static List<Account> createAccounts(int numAccounts) {
+		
+		List<Account> accounts = new ArrayList<>();
+		
+		while (numAccounts > 0) {
+			Account a = new Account(1000+ numAccounts, "savings");
+			accounts.add(a);
+			numAccounts--; 
+		}
+		
+		return accounts;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Account [id: " + this.id + "]";
+	}
+}
